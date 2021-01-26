e5583e15d9fe8b8ec2d9d894e868d6b1c155cfec
aronsommer <aronsommer@gmail.com>
2021-01-26 07:42:10 +0100

    first upload

---
+++ ExampleLeftRight/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform Target;
+    bool Follow;
+
+    Transform myTransform;
+    void Awake()
+    {
+        myTransform = transform;
+    }
+
+    void Start()
+    {
+        Follow = true;
+    }
+
+    void LateUpdate()
+    {
+        if (Target != null && Follow == true)
+        {
+            myTransform.position = new Vector3(Target.position.x, myTransform.position.y, myTransform.position.z);
+            myTransform.eulerAngles = new Vector3(15, 0, Target.eulerAngles.z);
+        }
+    }
+}
